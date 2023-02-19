acfa33c6f31a30451638ca48b5ac81dff075481b
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-02-19 17:17:55 -0300

    refactor: use electron-css-injector

--- packages/nunito/index.js
+++ packages/nunito/index.js
@@ -1,27 +1,9 @@
 const path = require("path")
+const { injectCSS } = require("electron-css-injector")
+
 const cssPath = path.resolve(__dirname, "./css/import.css")
 
-/**
- * @param {string} path
- */
-function injectFont() {
-    const links = Array.from(document.querySelectorAll("link"))
-    const css_exists = links.find(link => {
-        return (
-            path.normalize(link.getAttribute('href')).replace(/file:\\\\/g, "") ===
-            path.normalize(cssPath)
-        )
-    })
-
-    if (!css_exists) {
-        const css = document.createElement('link')
-        css.rel = "stylesheet"
-        css.href = cssPath
-        document.head.appendChild(css)
-    }
-}
-
-module.exports = {
+module.exports = Object.freeze({
     path: cssPath,
-    inject: injectFont
-}
+    inject: () => injectCSS(cssPath),
+})
