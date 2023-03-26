8211fc41b8741e756480ef9b75e3ab3b55c527fd
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-03-25 21:42:25 -0300

    fix: filenames

--- scripts/create/index.js
+++ scripts/create/index.js
@@ -37,7 +37,7 @@ function geerateIndexJs(packageDir) {
     const indexJsContent = `const path = require("path")
     const { injectCSS } = require("electron-css-injector")
     
-    const cssPath = path.resolve(__dirname, "./css/import.css")
+    const cssPath = path.resolve(__dirname, "./css/index.css")
     
     module.exports = Object.freeze({
         path: cssPath,
