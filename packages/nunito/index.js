c525f3dcea6d4d05aab199ef3e1cbc4309cacd40
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-03-26 00:02:07 -0300

    refactor: standardize nunito

--- packages/nunito/index.js
+++ packages/nunito/index.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const { injectCSS } = require("electron-css-injector")
 
-const cssPath = path.resolve(__dirname, "./css/import.css")
+const cssPath = path.resolve(__dirname, "./css/index.css")
 
 module.exports = Object.freeze({
     path: cssPath,
