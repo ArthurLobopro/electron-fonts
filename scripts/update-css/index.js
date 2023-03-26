5ed59208259281941fbbd19f75d50a02a9585cce
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-03-26 20:35:43 -0300

    feat: create script to auto-generate css files

---
+++ scripts/update-css/index.js
@@ -0,0 +1,110 @@
+const fs = require("fs")
+const path = require("path")
+
+function input(message = "", end = "\n") {
+    process.stdout.write(message)
+    process.stdout.write(end)
+
+    return new Promise(res => {
+        process.stdin.once("data", data => {
+            res(data.toString().trim())
+        })
+    })
+}
+
+async function getPackagePath() {
+    const packageName = await input("Package name: ")
+
+    if (!packageName) {
+        console.log("Package name is required.")
+        return getPackagePath()
+    }
+
+    const packagePath = path.normalize(path.resolve(__dirname, "../../packages/", packageName))
+
+    if (!fs.existsSync(packagePath)) {
+        console.log("Package not found.")
+        return getPackagePath()
+    }
+
+    return packagePath
+}
+
+async function main() {
+    const packagePath = await getPackagePath()
+    const packageJsonPath = path.resolve(packagePath, "package.json")
+
+    const packageName = String(require(packageJsonPath).name).split("/").pop()
+
+    const fontName = packageName
+        .replace(/^[a-z]/g, match => match.toUpperCase())
+        .replace(/-[a-z]/g, match => " " + match[1].toUpperCase())
+
+    const font_dir = path.resolve(packagePath, "fonts")
+
+    const fontFiles = fs.readdirSync(font_dir)
+
+    if (!fontFiles.length) {
+        console.log("No fonts found.")
+        return
+    }
+
+    const css_dir = path.resolve(packagePath, "css")
+
+    const relative_dir = path.relative(css_dir, font_dir)
+
+    const css_content = fontFiles.map(fileName => {
+        return {
+            name: fontName,
+            url: path.join(relative_dir, fileName),
+            style: getFontStyle(fileName),
+            weight: getFontWeigth(fileName)
+        }
+    })
+        .sort((a, b) => (
+            a.weight === b.weight ? a.style.localeCompare(b.style) * -1 :
+                a.weight - b.weight
+        ))
+        .map(font => {
+            return [
+                "@font-face {",
+                `    font-family: "${font.name}";`,
+                `    src: url(${font.url}) format("truetype");`,
+                `    font-style: ${font.style};`,
+                `    font-weight: ${font.weight};`,
+                "}"
+            ].join("\n")
+        }).join("\n\n")
+
+    fs.writeFileSync(path.resolve(css_dir, "index.css"), css_content)
+
+    console.log("Done.")
+    process.exit(0)
+}
+
+/**
+ * @param {string} fontName
+ */
+function getFontStyle(fontName) {
+    return fontName.toLowerCase().includes("italic") ? "italic" : "normal"
+}
+
+/**
+ * @param {string} fontName
+ */
+function getFontWeigth(fontName) {
+    const name = fontName.toLowerCase()
+
+    return name.includes("thin") ? 100 :
+        name.includes("extralight") ? 200 :
+            name.includes("light") ? 300 :
+                name.includes("regular") ? 400 :
+                    name.includes("medium") ? 500 :
+                        name.includes("semibold") ? 600 :
+                            name.includes("bold") ? 700 :
+                                name.includes("extrabold") ? 800 :
+                                    name.includes("black") ? 900 :
+                                        400
+}
+
+main()
