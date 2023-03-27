259a8e20721ef2fa17ab39bdef4fdf85f7c87338
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-03-26 21:22:00 -0300

    fix: standardize farsan files

--- packages/farsan/index.d.ts
+++ packages/farsan/index.d.ts
@@ -1,5 +1,4 @@
-
-    declare module "@electron-fonts/farsan" {
-        export const path: string
-        export function inject(): void
-    }
+declare module "@electron-fonts/farsan" {
+    export const path: string
+    export function inject(): void
+}
