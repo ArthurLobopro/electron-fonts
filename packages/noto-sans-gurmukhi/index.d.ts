f780dccf9e386d6bc7e4f18c0e5ff675be46f2a0
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-03-28 10:30:24 -0300

    feat: Added noto-sans-gurmukhi package

---
+++ packages/noto-sans-gurmukhi/index.d.ts
@@ -0,0 +1,4 @@
+declare module "@electron-fonts/noto-sans-gurmukhi" {
+    export const path: string
+    export function inject(): void
+}
---
+++ packages/noto-sans-gurmukhi/index.js
@@ -0,0 +1,9 @@
+const path = require("path")
+const { injectCSS } = require("electron-css-injector")
+
+const cssPath = path.resolve(__dirname, "./css/index.css")
+
+module.exports = Object.freeze({
+    path: cssPath,
+    inject: () => injectCSS(cssPath),
+})
