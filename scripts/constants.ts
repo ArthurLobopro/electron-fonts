7d34ee364751f6d18522a7d626950feb8cfb2472
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-12-07 20:50:33 -0300

    refactor: export things as constants

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,4 +1,12 @@
 import path from "node:path"
+import { api_key } from "./Util"
 
 export const base_version = "1.1.0"
 export const root = path.resolve(__dirname, "../")
+
+export const google_fonts_request = `https://webfonts.googleapis.com/v1/webfonts?sort=ALPHA&key=${api_key}`
+export const axios_options = {
+    headers: {
+        "Accept": "application/json"
+    }
+}
