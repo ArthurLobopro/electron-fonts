eefde1dd813efd78e4dc04f5d53b04d3b51c228f
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-02-17 18:42:24 -0300

    Update index.ts

--- scripts/republish-all/index.ts
+++ scripts/republish-all/index.ts
@@ -1,9 +1,20 @@
 import axios from "axios"
 import { execSync } from "node:child_process"
+import fs from "node:fs"
+import path from "node:path"
 import { argExists } from "../Util"
 import { axios_options, google_fonts_request, root } from "../constants"
 
 const dry = argExists("--dry")
+const publishedListPath = path.resolve(__dirname, "published.txt")
+
+if (!fs.existsSync(publishedListPath)) {
+    fs.writeFileSync(publishedListPath, "")
+}
+
+function alreadyPublished(family: string) {
+    return fs.readFileSync(publishedListPath).toString().includes(family)
+}
 
 axios.get(
     google_fonts_request,
@@ -21,25 +32,33 @@ axios.get(
             })
 
         for (const data of fontData) {
-            const {
-                family,
-            } = data
+            const { family } = data
 
-            process.stdout.write(`Adding "${family}"...`)
+            if (!alreadyPublished(family)) {
+                try {
+                    process.stdout.write(`Adding "${family}"...`)
 
-            execSync(
-                [
-                    "npm run add-package --",
-                    `--name="${family}"`,
-                    dry ? "--dry" : ""
-                ].join(" "),
-                {
-                    stdio: "ignore",
-                    cwd: root,
+                    execSync(
+                        [
+                            "npm run add-package --",
+                            `--name="${family}"`,
+                            dry ? "--dry" : ""
+                        ].join(" "),
+                        {
+                            stdio: "ignore",
+                            cwd: root,
+                        }
+                    )
+
+                    console.log("  Done!")
+
+                    execSync(`echo "${family}" >> ${publishedListPath}`)
+                } catch (error) {
+                    console.log("  Error!")
                 }
-            )
-
-            console.log("  Done!")
+            } else {
+                console.log(`${family} already published, skipping!`)
+            }
         }
 
         process.exit(0)
